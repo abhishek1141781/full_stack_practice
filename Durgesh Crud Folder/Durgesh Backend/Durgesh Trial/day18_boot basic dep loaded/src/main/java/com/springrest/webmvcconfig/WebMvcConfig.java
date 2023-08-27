ee4064b068a1c8477dce91ba3a370000f190e043
Abhishek <abhishek1141781@gmail.com>
2023-08-27 10:07:42 +0530

    more frontend

--- Durgesh Crud Folder/Durgesh Backend/Durgesh Trial/day18_boot basic dep loaded/src/main/java/com/springrest/webmvcconfig/WebMvcConfig.java
+++ Durgesh Crud Folder/Durgesh Backend/Durgesh Trial/day18_boot basic dep loaded/src/main/java/com/springrest/webmvcconfig/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:3000") // Change this to the appropriate origin
+            .allowedOrigins("http://localhost:3000", "http://192.168.0.105:3000", "http://192.168.0.103:3000","http://192.168.0.103") // Change this to the appropriate origin
             .allowedMethods("GET", "POST", "PUT", "DELETE")
             .allowCredentials(true);
     }
